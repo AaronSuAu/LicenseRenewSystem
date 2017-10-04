be1357b9f355b2f92446b99663df58d77afd7808
yangyjRex <yangyjrex@gmail.com>
2017-10-04 18:55:45 +1100

    add licid to renewalNoticeDao

--- src/main/java/com/comp9322/AssignREST/dao/RenewalNoticeDao.java
+++ src/main/java/com/comp9322/AssignREST/dao/RenewalNoticeDao.java
@@ -18,31 +18,30 @@ import com.comp9322.AssignREST.model.RenewalNotices;
 public interface RenewalNoticeDao {
 	@Select("select * from renewal_notices where status <> 'archived'")
 	List<RenewalNotices> getAll();
-	
+
 	@Insert("insert into renewal_notices (address, contact_email, "
-			+ "status, review_result, access_token)values("
-			+ "#{address}, #{contact_email}, #{status}, #{review_result}, "
-			+ "#{access_token})")
+			+ "status, review_result, access_token, licid)values("
+			+ "#{address}, #{contact_email}, #{status}, #{review_result}, " + "#{access_token}, " + "#{licid})")
 	@Options(useGeneratedKeys = true, keyProperty = "nid", keyColumn = "renewal_notice.nid")
 	int create(RenewalNotices renewalNotices);
-	
+
 	@Update("update renewal_notices set address=#{address}, contact_email ="
 			+ "#{contact_email}, status = #{status}, review_result = #{review_result}, "
 			+ "access_token = #{access_token}, licid=#{licid} where nid=#{nid}")
 	int update(RenewalNotices renewalNotices);
-	
+
 	@Select("select * from renewal_notices where status = #{status} and status <> 'archived'")
 	List<RenewalNotices> getNoticesByStatus(@Param("status") String status);
-	
+
 	@Select("select * from renewal_notices where nid = #{nid} and status <> 'archived'")
 	List<RenewalNotices> getNoticesByNid(@Param("nid") int nid);
-	
+
 	@Delete("delete from renewal_notices where nid = #{nid}")
 	int deleteNoticesByNid(@Param("nid") int nid);
-	
+
 	@Select("select * from renewal_notices where access_token = #{access_token} and status <> 'archived'")
 	List<RenewalNotices> getNoticesByAccessToken(@Param("access_token") String accessToken);
-	
+
 	@Delete("delete from renewal_notices where nid != null")
 	int deleteAll();
 }
